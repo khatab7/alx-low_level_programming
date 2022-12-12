36aa386efb5a02ebbbc8c2b412f5f5b329a9e151
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 <katab.abden8@gmail.com>
2022-12-11 18:34:55 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int nun;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 48; num < 58; num++)
 	{
-		putchar(num + '0');
-		if (num < 9)
+		putchar(num);
+		if (num != 57)
 		{
 			putchar(',');
 			putchar(' ');
