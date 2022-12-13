f42f2a30f68bd565f25d73dc57e705cba8e37916
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 <katab.abden8@gmail.com>
2022-12-12 17:58:16 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-		int num;
+	int num;
 
-		for (num - 0; num < 58; num++)
+	for (num = 0; num <= 9 ; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
 		{
-		putchar(num);
-		if (num != 57)
-		{
-		putchar(',');
-		putchar(' ');
+			putchar(' , ');
+			putchar(' ');
 		}
-		}
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
