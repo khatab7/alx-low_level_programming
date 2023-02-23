fa68fa01d52cad4e4b01ff6ad5d5472a6d81cc24
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 <katab.abden8@gmail.com>
2023-02-23 14:07:03 -0800

    C - Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints the name of the file
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-int ain(void)
+ 
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
