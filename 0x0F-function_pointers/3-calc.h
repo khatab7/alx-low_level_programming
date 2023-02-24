be2f06faefc99525a8169e90e851ccc23d4f603d
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 <katab.abden8@gmail.com>
2023-02-24 06:24:52 -0800

    Function pointers

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -11,7 +11,9 @@ typedef struct op
 
 	int (*f)(int a, int b);
 
-}op_t;
+}
+
+op_t;
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -21,4 +23,3 @@ int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif
-
