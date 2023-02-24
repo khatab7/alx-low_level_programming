35a16eaa4295cde91c935c5641942822e14caf24
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 <katab.abden8@gmail.com>
2023-02-24 06:31:09 -0800

    Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,10 +13,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 		return;
-
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
-
